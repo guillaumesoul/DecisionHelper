b69dbb88d625b726aa059d14edf71f2ce5490694
guillaumesoul <guillaume.soullard@gmail.com>
2015-02-25 22:45:30 +0100

    parameter form validation

--- public/js/decision/create.js
+++ public/js/decision/create.js
@@ -1,15 +1,60 @@
 $(document).ready(function(){
 
-    // configure for module loader
-
-
     //$('#parameterForm').parsley();
+    //$('#parameterForm').isValid();
 
-    /*$('#parameterForm').submit(function(event) {
-        var mydata = $('#parameterForm').serializeArray();
-        $('#parametersList').html("pipou");
-    });*/
-    $('.addParameter').click(function(event) {
+    $('#parameterForm').submit(function(e) {
+        e.preventDefault();
+        if ( $(this).parsley().isValid() ) {
+            var parameterData = new Array();
+            var parameterName = $('.parameterName').val();
+            var parameterMinValue = $('.parameterMinValue').val();
+            var parameterMaxValue = $('.parameterMaxValue').val();
+            var parameterUnit = $('.parameterUnit').val();
+            //parameterData.push(parameterName,parameterValue,parameterMinValue,parameterMaxValue,parameterUnit);
+
+            $.ajax({
+                type : "POST",
+                url : './getParameterData',
+                data: {
+                    parameterName: parameterName,
+                    parameterMinValue: parameterMinValue,
+                    parameterMaxValue: parameterMaxValue,
+                    parameterUnit: parameterUnit
+                },
+                success : function(data,xhr) {
+                    var parsedData = JSON.parse(data);
+
+                    //allParametersData.push(parsedData);
+                    var myhtml = setDisplayParameterData(parsedData);
+                    $('#parametersList').append(myhtml);
+                    var enfant = $("#parametersList").children();
+                }
+            });
+        }
+    });
+
+    $('#parameterForm').submit(function(e) {
+        e.preventDefault();
+        if ( $(this).parsley().isValid() ) {
+            $.ajax({
+                type : "POST",
+                url : './createDecision',
+                data: {
+                },
+                success : function(data,xhr) {
+
+                }
+            });
+        }
+    });
+
+    $.listen('parsley:form:success', function(e){
+
+
+    });
+
+    /*$('.addParameter').click(function(event) {
 
         var parameterData = new Array();
 
@@ -41,7 +86,7 @@ $(document).ready(function(){
             }
         });
 
-    });
+    });*/
 
     /*
     * Get the parameters data for all the parameters previously entered by the user
@@ -65,7 +110,6 @@ $(document).ready(function(){
 function setDisplayParameterData(parameterData){
     var htmlCode = '<div class="parameterData col-xs-3">';
     htmlCode += ("<label>NAME : </label><span class='paramName'>"+parameterData["parameterName"]+"</span><br>");
-    //htmlCode += ("<label>VALUE : </label><span>"+parameterData["parameterValue"]+"</span><br>");
     htmlCode += ("<label>MIN VALUE : </label><span class='paramMinValue'>"+parameterData["parameterMinValue"]+"</span><br>");
     htmlCode += ("<label>MAX VALUE : </label><span class='paramMaxValue'>"+parameterData["parameterMaxValue"]+"</span><br>");
     htmlCode += ("<label>UNIT : </label><span class='paramUnit'>"+parameterData["parameterUnit"]+"</span><br>");
