b7219b7a8d6d2e2786829bced0a2e3a33c45d477
guillaumesoul <guillaume.soullard@gmail.com>
2015-03-22 22:04:55 +0100

    graphe multi series OK

--- public/js/decision/table.js
+++ public/js/decision/table.js
@@ -17,10 +17,36 @@ $(document).ready(function() {
 
     var paramatersData;
 
-    $('.saveParametersInput').click( function() {
+    $("#chartGeneration").click(function(){
+
+
+        var allDataRows = $('.parametersSerie');
+        var allParametersSerie = new Array();
+        var parametersSerie = new Array();
+        var parameterData = new Array();
+        var calculatedParameterValue;
+
+        allDataRows.each(function(i){
+            var singleSerie = $(this)[0].getElementsByClassName('parameterValue');
+            for (var i=0; i<singleSerie.length; i++){
+                parameterData['parameterId'] = singleSerie[i].getAttribute("parameterId");
+                parameterData['parameterName'] = singleSerie[i].getAttribute("parameterName");
+                parameterData['parameterMinValue'] = singleSerie[i].getAttribute("parameterMinValue");
+                parameterData['parameterMaxValue'] = singleSerie[i].getAttribute("parameterMaxValue");
+                parameterData['parameterValue'] = singleSerie[i].value;
+                calculatedParameterValue = calculateParameterData(parameterData);
+                parameterData['calculatedParameterValue'] = calculatedParameterValue;
+                parametersSerie.push(parameterData);
+                parameterData = [];
+            }
+            allParametersSerie.push(parametersSerie);
+            parametersSerie = [];
+        });
+
+        setEchartOption2(allParametersSerie);
 
         //Recuperation des inputs
-        var data = $('.parameterValue');
+        /*var data = $('.parameterValue');
         var parameterDataList = new Array();
         var parameterData = new Array();
         var calculatedParameterValue;
@@ -35,19 +61,11 @@ $(document).ready(function() {
             parameterDataList.push(parameterData);
             parameterData = [];
 
-        });
-        paramatersData = parameterDataList;
+        });*/
+        paramatersData = parametersSerie;
 
-        //Traitements des inputs calculs des valeurs
-        var calculatedDataInputList = $('.parameterCalculatedValue');
-        calculatedDataInputList.each(function(indice){
-            var calculatedParameterValue = parameterDataList[indice]["calculatedParameterValue"];
-            calculatedDataInputList[indice].value = calculatedParameterValue;
-        })
 
-    });
 
-    $("#chartRedirect").click(function(){
         require.config({
             paths: {
                 echarts: '/bower_components/echarts/build/dist'
@@ -62,16 +80,30 @@ $(document).ready(function() {
                 // Initialize after dom ready
                 var myChart = ec.init(document.getElementById('main'));
 
-                var options = setEchartOption(paramatersData);
+                //var options = setEchartOption(paramatersData);
+                var options = setEchartOption2(allParametersSerie);
                 // Load data into the ECharts instance
                 myChart.setOption(options);
+
+                /*permet de mettre a jour une série de valeur
+                var updatedSeries = myChart.getSeries();
+                updatedSeries[0]['name'] = "je change";
+                updatedSeries[0].data[0].value[0] = 100;
+                updatedSeries[0].data[0].value[1] = 100;
+                updatedSeries[0].data[0].value[2] = 100;
+                myChart.setSeries(updatedSeries);*/
             }
         );
 
     });
 
-
-
+    /*
+    * permet d'ajouter une ligne dans le tableau parametre en clonant la premiere ligne
+    * */
+    $('.addRow').click( function() {
+        var row_clone = $('#parameterTable tbody tr:eq(0)').clone();
+        parameterTable.fnAddData(row_clone[0]);
+    });
 });
 
 
@@ -102,36 +134,21 @@ function calculateParameterData(parameterData){
 * */
 function setEchartOption(paramatersData){
 
-    console.log(paramatersData);
-
     var titleString = "pipou";
     var polarString="[";
+    var dataString='[{"value": [';
     for	(var index = 0; index < paramatersData.length; index++) {
-        //console.log(paramatersData[index]['parameterName']);
-        if (index <= 4){
-            polarString += '{"text": "'+paramatersData[index]["parameterName"]+'", "max": 100},';
+        if (index == paramatersData.length-1 ){
+            polarString += '{"text": "'+paramatersData[index]["parameterName"]+'", "max": 100}]';
+            dataString += paramatersData[index]["calculatedParameterValue"]+'],"name":"serie1"}]';
+
         }else {
-            polarString += '{"text": "'+paramatersData[index]["parameterName"]+'", "max": 100}';
+            polarString += '{"text": "'+paramatersData[index]["parameterName"]+'", "max": 100},';
+            dataString += paramatersData[index]["calculatedParameterValue"]+',';
         }
     }
-    polarString += "]";
     var JSONPolar = JSON.parse(polarString);
-
-    var dataString='[{';
-    for	(var i = 0; i < paramatersData.length; i++) {
-        console.log(paramatersData[i]);
-        if (i == 0){
-            dataString += '"value": ['+paramatersData[i]["calculatedParameterValue"]+',';
-        }else if (i == paramatersData.length-1){
-            dataString += paramatersData[i]["calculatedParameterValue"]+'],"name":"serie1"}]';
-        }else{
-            dataString += paramatersData[i]["calculatedParameterValue"]+',';
-        }
-    }
-    var zaza = '[{"value": [100, 00, 100, 100, 100, 100],"name":"bon..."}]';
-
     var JSONSeries = JSON.parse(dataString);
-    console.log(JSONSeries);
 
     option = {
         title: {
@@ -172,11 +189,64 @@ function setEchartOption(paramatersData){
         ]
     };
 
+    return option;
+}
+
+/*
+* fonction permettant de definir les options pour un graphique de type radar
+* */
+function setEchartOption2(seriesList){
+
+    var polarString="[";
+    //var seriesString="[{";
+    //var dataString='[{"value": [';
+    var dataString='[';
+
+    for (var i = 0; i<seriesList.length; i++){
+        
+        console.log(seriesList[i]);
 
 
-    /*option = {
+        dataString += '{"value": [';
+        
+        var singleSerie = seriesList[i];
+        for ( var j = 0; j<singleSerie.length; j++){
+            /*console.log(singleSerie[j]);
+            console.log(singleSerie[j]["parameterName"]);
+            console.log(singleSerie[j]["parameterValue"]);*/
+
+            if ( i == 0 ){
+                if (j == singleSerie.length-1 ){
+                    polarString += '{"text": "'+singleSerie[j]["parameterName"]+'", "max": 100}';
+
+                }else {
+                    polarString += '{"text": "'+singleSerie[j]["parameterName"]+'", "max": 100},';
+                }
+            }
+
+            if (j == singleSerie.length-1 ){
+                if (j == singleSerie.length-1 && i == seriesList.length-1 ){
+                    polarString += "]";
+                    dataString += singleSerie[j]["calculatedParameterValue"]+'],"name":"serie'+i+'"}';
+                }else{
+                    dataString += singleSerie[j]["calculatedParameterValue"]+'],"name":"serie'+i+'"},';
+                }
+            }else {
+                dataString += singleSerie[j]["calculatedParameterValue"]+',';
+            }
+        }
+    }
+
+    dataString += "]";
+    console.log(polarString);
+    console.log(dataString);
+
+    var JSONPolar = JSON.parse(polarString);
+    var JSONSeries = JSON.parse(dataString);
+
+    option = {
         title: {
-            text: '（Budget vs spending）',
+            text: "mon titre",
             subtext: ''
         },
         tooltip: {
@@ -186,7 +256,7 @@ function setEchartOption(paramatersData){
             orient: 'vertical',
             x: 'right',
             y: 'bottom',
-            data: ['（Allocated Budget）', '（Actual Spending）']
+            data: ['a changer']
         },
         toolbox: {
             show: true,
@@ -200,36 +270,40 @@ function setEchartOption(paramatersData){
         polar: [
             {
                 scale: true,
-                indicator: [
-                    {text: '（sales）', min: 0, max: 1000},
-                    {text: '（Administration）', min: 0, max: 1000},
-                    {text: '（Information Techology）', min: 0, max: 1000},
-                    {text: '（Customer Support）', min: 0, max: 1000},
-                    {text: '（Development）', min: 0, max: 1000},
-                    {text: '（Marketing）', min: 0, max: 1000}
-                ]
+                indicator: JSONPolar
             }
         ],
-        calculable: true,
+        //calculable: true,
         series: [
             {
                 name: '（Budget vs spending）',
                 type: 'radar',
-                data: [
-                    {
-                        value: [500, 610 , 343, 100, 850, 492],
-                        name: '（Allocated Budget）'
-                    },
-                    {
-                        value: [1000, 1000, 1000, 1000, 1000, 1000],
-                        name: '（Actual Spending）'
-                    }
-                ]
+                data: JSONSeries
             }
         ]
-    };*/
+    };
 
     return option;
+
+
+
+    /*series: [
+        {
+            name: '（Budget vs spending）',
+            type: 'radar',
+            data: [
+                {
+                    value: [4300, 10000, 28000, 35000, 50000, 19000],
+                    name: '（Allocated Budget）'
+                },
+                {
+                    value: [5000, 14000, 28000, 31000, 42000, 21000],
+                    name: '（Actual Spending）'
+                }
+            ]
+        }
+    ]*/
+
 }
 
 
